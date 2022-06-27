6fd75291e335ac687ba20dba2423a0d8e8bd91eb
Cardorel <54881922+Cardorel@users.noreply.github.com>
2022-06-27 18:53:35 +0200

    Deleted unnecessary files

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "./layout.module.css";
-import utilStyles from "../styles/utils.module.css";
-import Link from "next/link";
 import Header from "./header";
 
-const name = "[Your Name]";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Bupa Test";
 
 export default function Layout({ children }) {
   return (

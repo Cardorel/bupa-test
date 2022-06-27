811973c18123c7b57a49fa1f448399013619bff2
Cardorel <54881922+Cardorel@users.noreply.github.com>
2022-06-27 18:25:15 +0200

    changed className

--- components/header.js
+++ components/header.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div class="toolbar" role="banner">
-      <div class="toolbar-content">
-        <span class="logo">BT</span>
-        <span class="logo-text">Bupa Test</span>
+    <div className="toolbar" role="banner">
+      <div className="toolbar-content">
+        <span className="logo">BT</span>
+        <span className="logo-text">Bupa Test</span>
       </div>
     </div>
   );
